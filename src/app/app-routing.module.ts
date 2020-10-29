28b5fbfd6d3634040fdf588a07d60baa668b8539
Fede <dfedericovera@gmail.com>
2020-10-29 20:05:43 -0300

    agrego algunas animaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,22 @@ import { MenuPrincipalComponent } from './pages/menu-principal/menu-principal.co
 import { SignupAdministratorComponent } from './pages/signup-administrator/signup-administrator.component';
 import { SignupPatientComponent } from './pages/signup-patient/signup-patient.component';
 import { SignupProfessionalComponent } from './pages/signup-professional/signup-professional.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 const routes: Routes = [
   {path: '',component: LoginComponent},
-  {path: 'login',component: LoginComponent},
-  {path: 'signupPatient',component: SignupPatientComponent},
-  {path: 'signupProfessional',component: SignupProfessionalComponent},
-  {path: 'signupAdministrator',component: SignupAdministratorComponent},
-  {path: 'home',component: MenuPrincipalComponent},
+  {path: 'login',component: LoginComponent, data: {animation: 'login'} },
+  {path: 'signupPatient',component: SignupPatientComponent, data: {animation: 'signupPatient'} },
+  {path: 'signupProfessional',component: SignupProfessionalComponent,data: {animation: 'signupProfessional'}},
+  {path: 'signupAdministrator',component: SignupAdministratorComponent,data: {animation: 'signupAdministrator'}},
+  {path: 'home',component: MenuPrincipalComponent,data: {animation: 'home'}},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
