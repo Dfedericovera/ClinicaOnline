7094f2c7f2e3b85f0c6cd05501099949669f10d1
Fede <dfedericovera@gmail.com>
2020-11-05 19:43:57 -0300

    Agrego validacion de fecha a partir del dia de hoy

--- src/app/components/appointment-form/appointment-form.component.ts
+++ src/app/components/appointment-form/appointment-form.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentFormComponent implements OnInit {
 
+  minDate = new Date().getFullYear()+'-'+('0' + (new Date().getMonth() + 1)).slice(-2)+'-'+('0' + new Date().getDate()).slice(-2);
+
   constructor() { }
 
   ngOnInit(): void {
