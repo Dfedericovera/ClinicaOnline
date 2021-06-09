0a3ee6d18a5f2f9d2aabf44f8e5300a4e8360e7a
Fede <dfedericovera@gmail.com>
2021-06-09 03:39:58 -0300

    vista pagina usuarios

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -5,11 +5,69 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.sass']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit
+{
+  userInfo: boolean;
+  approveProfessional: boolean;
+  formAdministrator: boolean;
+  formPatient: boolean;
+  formProfessional: boolean;
 
-  constructor() { }
+  constructor()
+  {
+    this.userInfo = true;
+    this.approveProfessional = false;
+    this.formAdministrator = false;
+    this.formPatient = false;
+    this.formProfessional = false;
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+  }
+
+  showUserInfo()
+  {
+    this.userInfo = true;
+    this.approveProfessional = false;
+    this.formAdministrator = false;
+    this.formPatient = false;
+    this.formProfessional = false;
+  }
+
+  showApproveProfessional()
+  {
+    this.userInfo = false;
+    this.approveProfessional = true;
+    this.formAdministrator = false;
+    this.formPatient = false;
+    this.formProfessional = false;
+  }
+
+  showForms(who)
+  {
+    this.userInfo = false;
+    this.approveProfessional = false;
+
+    if (who == "administrator")
+    {
+      this.formAdministrator = true;
+      this.formProfessional = false;
+      this.formPatient = false;
+    }
+    else if (who == "professional")
+    {
+      this.formAdministrator = false;
+      this.formProfessional = true;
+      this.formPatient = false;
+    }
+    else
+    {
+      this.formAdministrator = false;
+      this.formProfessional = false;
+      this.formPatient = true;
+    }
+
   }
 
 }
