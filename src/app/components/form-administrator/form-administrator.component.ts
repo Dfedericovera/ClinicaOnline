249ed0a3143c0e1fb61efeb960ebbcf2cae10bf5
Fede <dfedericovera@gmail.com>
2021-06-09 02:09:59 -0300

    Formulario administrador + pagina usuarios + servicio administrador

--- src/app/components/form-administrator/form-administrator.component.ts
+++ src/app/components/form-administrator/form-administrator.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Specialty } from 'src/app/clases/specialty';
+import { FileI } from 'src/app/interface/file';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProfessionalService } from 'src/app/services/professional.service';
+import { SpecialtyService } from 'src/app/services/specialty-service';
 
 @Component({
   selector: 'app-form-administrator',
@@ -7,9 +14,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormAdministratorComponent implements OnInit {
 
-  constructor() { }
+  administratorForm: FormGroup;
+  photo1: FileI;
+  photos: Array<FileI>;
+  registered: boolean;
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private profesionalService: ProfessionalService,
+    private router: Router,
+  )
+  {
+    this.registered = false;
+    this.photos = new Array();
+  }
+
+  ngOnInit(): void
+  {
+    this.createForm();
+  }
+
+  createForm()
+  {
+    this.administratorForm = this.fb.group({
+      id: [""],
+      name: ["", Validators.required],
+      dni: ["", [Validators.required, Validators.minLength(7)]],
+      age: ["", Validators.required],
+      lastName: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
+      specialty: [Array, Validators.required],
+      approved: [""],
+    });
+  }
+
+  verError(){
+    console.log(this.administratorForm );
+    
+  }
+
+  onSubmit()
+  {
+    try
+    {
+      /* console.log(this.administratorForm.value); */
+      this.assignPhotos();
+      this.authService.register(this.administratorForm.controls.email.value, this.administratorForm.controls.password.value).then(user =>
+      {
+        if (user)
+        {
+          this.administratorForm.controls['id'].setValue(user.uid);
+          this.administratorForm.controls['approved'].setValue(false);
+          this.profesionalService.createProfessional(this.administratorForm.value, this.photos).then(patient =>
+          {
+            console.log('Professional Created', patient);
+            user.updateProfile({
+              displayName: patient.name,
+            }).then(() =>
+            {
+              console.log('Now Verify your email to login.');
+              this.registered = true;
+            })
+          });
+        }
+      }).catch(error => { console.log('Error', error); });
+
+    } catch (error)
+    {
+      console.error(error);
+    }
+  }
+
+  handlePhoto1(file)
+  {
+    this.photo1 = file.target.files[0];
+  }
+
+  assignPhotos()
+  {
+    if (this.photo1)
+    {
+      this.photos.push(this.photo1);
+    }
   }
 
 }
