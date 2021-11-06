1fbec17172f2ed481c1a2852ab01a3dfa2baf0e5
dfedericovera@gmail.com <dfedericovera@gmail.com>
2021-11-06 18:16:41 -0300

    saco disponibilidad

--- src/app/clases/specialty.ts
+++ src/app/clases/specialty.ts
@@ -1,8 +1,11 @@
+import { DisponibilidadHoraria } from "../interface/disponibilidad-horaria";
+
 export class Specialty{
 
     id:string;
     specialty:string;
     duration:number;
+    public disponibilidadHoraria:DisponibilidadHoraria;
 
     public constructor(init?: Partial<Specialty>) {
         if(init){
