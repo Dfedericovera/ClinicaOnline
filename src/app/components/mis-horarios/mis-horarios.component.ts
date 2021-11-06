4579714deac33eba8c1e1cac5349a6a6632fbd2b
dfedericovera@gmail.com <dfedericovera@gmail.com>
2021-11-06 18:17:00 -0300

    Fis disponibilidad segun especialidad

--- src/app/components/mis-horarios/mis-horarios.component.ts
+++ src/app/components/mis-horarios/mis-horarios.component.ts
@@ -1,10 +1,16 @@
-import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Professional } from 'src/app/clases/professional';
 import { Specialty } from 'src/app/clases/specialty';
 import { ProfessionalService } from 'src/app/services/professional.service';
 import { SpecialtyService } from 'src/app/services/specialty-service';
-
+enum DEFAULTHOURS
+{
+  entradaSabado = '08:00',
+  salidaSabado = '14:00',
+  entradaSemana = '08:00',
+  salidaSemana = '18:00'
+}
 @Component({
   selector: 'app-mis-horarios',
   templateUrl: './mis-horarios.component.html',
@@ -12,6 +18,7 @@ import { SpecialtyService } from 'src/app/services/specialty-service';
 })
 export class MisHorariosComponent implements OnInit
 {
+
 
   horarioForm: FormGroup;
   showForm: boolean = false;
@@ -21,6 +28,7 @@ export class MisHorariosComponent implements OnInit
   ngSalidaSemana: string;
   ngEntradaSabado: string;
   ngSalidaSabado: string;
+  indexSpecialtySelected: number = 0;
   @Input() user: Professional;
 
   constructor(
@@ -28,48 +36,63 @@ export class MisHorariosComponent implements OnInit
     private professionalService: ProfessionalService,
     private specialtyService: SpecialtyService
   )
-  {
-
-  }
+  { }
 
   ngOnInit(): void
   {
     this.getSpecialtys();
     this.createForm();
-    this.cargarHorarios();
+    this.cargarHorarios()
   }
 
   getSpecialtys()
   {
-    this.specialtyService.getSpecialtys().subscribe(v =>
-    {
-      this.specialtys = v;
-    })
+    this.specialtys = this.user.specialty;
   }
 
   cargarHorarios()
   {
-    let ESem = new Date(this.user.disponibilidadHoraria.EntradaSemana).getHours().toString() + ':' + new Date(this.user.disponibilidadHoraria.EntradaSemana).getMinutes().toString();
-    let SSem = new Date(this.user.disponibilidadHoraria.SalidaSemana).getHours().toString() + ':' + new Date(this.user.disponibilidadHoraria.SalidaSemana).getMinutes().toString();
-    let ESab = new Date(this.user.disponibilidadHoraria.EntradaSabado).getHours().toString() + ':' + new Date(this.user.disponibilidadHoraria.EntradaSabado).getMinutes().toString();
-    let SSab = new Date(this.user.disponibilidadHoraria.SalidaSabado).getHours().toString() + ':' + new Date(this.user.disponibilidadHoraria.SalidaSabado).getMinutes().toString();
+    console.log(typeof(this.specialtys[this.indexSpecialtySelected].disponibilidadHoraria));
+    
+    if (typeof(this.specialtys[this.indexSpecialtySelected].disponibilidadHoraria) == 'undefined')
+    {
+      this.ngEntradaSemana = DEFAULTHOURS.entradaSemana;
+      this.ngSalidaSemana = DEFAULTHOURS.salidaSemana;
+      this.ngEntradaSabado = DEFAULTHOURS.entradaSabado;
+      this.ngSalidaSabado = DEFAULTHOURS.salidaSabado;
+    }
+    else
+    {
+      let ESem = new Date(this.user.specialty[this.indexSpecialtySelected].disponibilidadHoraria.EntradaSemana).getHours().toString() + ':' + this.validarMinutos(new Date(this.user.specialty[0].disponibilidadHoraria.EntradaSemana));
+      let SSem = new Date(this.user.specialty[this.indexSpecialtySelected].disponibilidadHoraria.SalidaSemana).getHours().toString() + ':' + this.validarMinutos(new Date(this.user.specialty[0].disponibilidadHoraria.SalidaSemana));
+      let ESab = new Date(this.user.specialty[this.indexSpecialtySelected].disponibilidadHoraria.EntradaSabado).getHours().toString() + ':' + this.validarMinutos(new Date(this.user.specialty[0].disponibilidadHoraria.EntradaSabado));
+      let SSab = new Date(this.user.specialty[this.indexSpecialtySelected].disponibilidadHoraria.SalidaSabado).getHours().toString() + ':' + this.validarMinutos(new Date(this.user.specialty[0].disponibilidadHoraria.SalidaSabado));
+      this.ngEntradaSemana = ESem;
+      this.ngSalidaSemana = SSem;
+      this.ngEntradaSabado = ESab;
+      this.ngSalidaSabado = SSab;
+    }
 
-    this.ngEntradaSemana = ESem;
-    this.ngSalidaSemana = SSem;
-    this.ngEntradaSabado = ESab;
-    this.ngSalidaSabado = SSab;
+  }
 
+  validarMinutos(date: Date)
+  {
+    if (date.getMinutes() < 10)
+    {
+      return '0' + date.getMinutes().toString();
+    }
+    return date.getMinutes().toString();
   }
 
   createForm()
   {
-    if (this.user.disponibilidadHoraria != undefined)
+    if (this.user.specialty[0].disponibilidadHoraria != undefined)
     {
       this.horarioForm = this.fb.group({
-        horarioEntradaSab: [this.user.disponibilidadHoraria.EntradaSemana, [Validators.required]],
-        horarioSalidaSab: [this.user.disponibilidadHoraria.SalidaSemana, [Validators.required]],
-        horarioEntrada: [this.user.disponibilidadHoraria.EntradaSabado, [Validators.required]],
-        horarioSalida: [this.user.disponibilidadHoraria.SalidaSabado, [Validators.required]]
+        horarioEntradaSab: [new Date(this.user.specialty[0].disponibilidadHoraria.EntradaSemana), [Validators.required]],
+        horarioSalidaSab: [new Date(this.user.specialty[0].disponibilidadHoraria.SalidaSemana), [Validators.required]],
+        horarioEntrada: [new Date(this.user.specialty[0].disponibilidadHoraria.EntradaSabado), [Validators.required]],
+        horarioSalida: [new Date(this.user.specialty[0].disponibilidadHoraria.SalidaSabado), [Validators.required]]
       });
     }
     else
@@ -85,23 +108,24 @@ export class MisHorariosComponent implements OnInit
 
   onChooseSpecialty(specialty: Specialty)
   {
-
+    this.specialtys.forEach((v, i) =>
+    {
+      if (specialty.id == v.id)
+      {
+        this.indexSpecialtySelected = i;
+      }
+    })
+    this.cargarHorarios();
   }
 
   onSubmit()
   {
-    this.user.disponibilidadHoraria = {
+    this.user.specialty[this.indexSpecialtySelected].disponibilidadHoraria = {
       EntradaSemana: this.horarioForm.controls['horarioEntrada'].value.getTime(),
       SalidaSemana: this.horarioForm.controls['horarioSalida'].value.getTime(),
       EntradaSabado: this.horarioForm.controls['horarioEntradaSab'].value.getTime(),
       SalidaSabado: this.horarioForm.controls['horarioSalidaSab'].value.getTime()
     }
-
-    /*     this.user.disponibilidadHoraria.EntradaSemana = this.horarioForm.controls['horarioEntrada'].value.getTime();
-        this.user.disponibilidadHoraria.SalidaSemana = this.horarioForm.controls['horarioSalida'].value.getTime();
-        this.user.disponibilidadHoraria.EntradaSabado = this.horarioForm.controls['horarioEntradaSab'].value.getTime();
-        this.user.disponibilidadHoraria.SalidaSabado = this.horarioForm.controls['horarioSalidaSab'].value.getTime();
-     */
     this.professionalService.editProfessional(this.user).then(v =>
     {
       this.isSubmitted = true;
