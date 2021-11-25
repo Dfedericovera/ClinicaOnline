2b6151c0058a905fb58631d3607f43882287e96d
dfedericovera@gmail.com <dfedericovera@gmail.com>
2021-11-25 02:27:25 -0300

    Agrego exportar excel

--- src/app/components/patient-list/patient-list.component.ts
+++ src/app/components/patient-list/patient-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Patient } from '../../clases/patient';
-
+import { AuthService } from 'src/app/services/auth.service';
+import * as XLSX from 'xlsx';
 @Component({
   selector: 'app-patient-list',
   templateUrl: './patient-list.component.html',
@@ -10,15 +11,33 @@ export class PatientListComponent implements OnInit {
 
   @Input() patients: Patient[];
   @Output() choosePatient: EventEmitter<Patient> = new EventEmitter<Patient>();
+  user:any;
 
-
-  constructor() { }
+  constructor(private AuthService: AuthService) { }
 
   ngOnInit(): void {
+    this.AuthService.user$.subscribe(value=>{
+      this.user = value;         
+    })
   }
 
   onChoosePatient(patient:Patient){
     this.choosePatient.emit(patient);
+  }
+  excel()
+  {
+    /* table id is passed over here */
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+    /* const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.patientsList); */
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    
+    /* save to file */
+    XLSX.writeFile(wb, "Usuarios.xlsx");
   }
 
 }
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -3,6 +3,8 @@ import { Patient } from 'src/app/clases/patient';
 import { Professional } from 'src/app/clases/professional';
 import { PatientService } from 'src/app/services/patient.service';
 import { ProfessionalService } from 'src/app/services/professional.service';
+import * as XLSX from 'xlsx';
+
 
 @Component({
   selector: 'app-users',
@@ -16,13 +18,13 @@ export class UsersComponent implements OnInit
   formAdministrator: boolean;
   formPatient: boolean;
   formProfessional: boolean;
-  patientsList:Patient[];
-  professionalList:Array<Professional>=[];
-  forms:boolean;
+  patientsList: Patient[];
+  professionalList: Array<Professional> = [];
+  forms: boolean;
 
   constructor(
-    private patientService:PatientService,
-    private professionalService:ProfessionalService
+    private patientService: PatientService,
+    private professionalService: ProfessionalService
   )
   {
     this.userInfo = true;
@@ -35,10 +37,12 @@ export class UsersComponent implements OnInit
 
   ngOnInit(): void
   {
-    this.patientService.getPatients().subscribe(value=>{
+    this.patientService.getPatients().subscribe(value =>
+    {
       this.patientsList = value.filter(user => user.usertype == "patient");
     });
-    this.professionalService.getProfessionals().subscribe(value=>{
+    this.professionalService.getProfessionals().subscribe(value =>
+    {
       this.professionalList = value.filter(user => user.usertype == "professional");
     })
 
@@ -63,7 +67,8 @@ export class UsersComponent implements OnInit
     this.formProfessional = false;
     this.forms = false;
   }
-  showform(){
+  showform()
+  {
     this.userInfo = false;
     this.approveProfessional = false;
     this.formAdministrator = false;
@@ -97,5 +102,46 @@ export class UsersComponent implements OnInit
     }
 
   }
+
+  excel()
+  {
+    /* table id is passed over here */
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+    /* const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.patientsList); */
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, "Usuarios.xlsx");
+  }
+
+//npm i --save exceljs file-saver
+/*   excel2()
+  {
+    let workbook = new Workbook();
+    let worksheet = workbook.addWorksheet('ProductSheet');
+
+    worksheet.columns = [
+      { header: 'Id', key: 'id', width: 10 },
+      { header: 'Name', key: 'name', width: 32 },
+      { header: 'Brand', key: 'brand', width: 10 },
+      { header: 'Color', key: 'color', width: 10 },
+      { header: 'Price', key: 'price', width: 10, style: { font: { name: 'Arial Black', size: 10 } } },
+    ];
+
+    this.data.forEach(e =>
+    {
+      worksheet.addRow({ id: e.id, name: e.name, brand: e.brand, color: e.color, price: e.price }, "n");
+    });
+
+    workbook.xlsx.writeBuffer().then((data) =>
+    {
+      let blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      fs.saveAs(blob, 'ProductData.xlsx');
+    })
+  } */
 
 }
